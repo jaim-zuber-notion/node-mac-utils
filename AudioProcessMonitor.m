b0419b2af4108b16558377e7f7c666d7ab5d008d
Jaim Zuber <jaim.zuber@makenotion.com>
2025-03-27 14:09:00 -0500

    switch tabs to 2 spaces

--- AudioProcessMonitor.m
+++ AudioProcessMonitor.m
@@ -6,107 +6,107 @@
 @implementation AudioProcessMonitor
 
 - (instancetype)init {
-    self = [super init];
-    if (self) {
-    }
-    return self;
+  self = [super init];
+  if (self) {
+  }
+  return self;
 }
 
 + (NSArray<NSString *> *)getRunningInputAudioProcesses:(NSError **)error {
-    AudioObjectPropertyAddress address = {
-        kAudioHardwarePropertyProcessObjectList,
-        kAudioObjectPropertyScopeGlobal,
-        kAudioObjectPropertyElementMain
-    };
-    
-    UInt32 dataSize = 0;
-    OSStatus status = AudioObjectGetPropertyDataSize(kAudioObjectSystemObject,
-                                                   &address,
-                                                   0,
-                                                   NULL,
-                                                   &dataSize);
-    
-    if (status != noErr) {
-        if (error) {
-            *error = [NSError errorWithDomain:@"AudioProcessMonitor"
-                                       code:status
-                                   userInfo:@{NSLocalizedDescriptionKey: @"Failed to get process list size"}];
-        }
-        return @[];
+  AudioObjectPropertyAddress address = {
+      kAudioHardwarePropertyProcessObjectList,
+      kAudioObjectPropertyScopeGlobal,
+      kAudioObjectPropertyElementMain
+  };
+
+  UInt32 dataSize = 0;
+  OSStatus status = AudioObjectGetPropertyDataSize(kAudioObjectSystemObject,
+                                                 &address,
+                                                 0,
+                                                 NULL,
+                                                 &dataSize);
+
+  if (status != noErr) {
+      if (error) {
+          *error = [NSError errorWithDomain:@"AudioProcessMonitor"
+                                     code:status
+                                 userInfo:@{NSLocalizedDescriptionKey: @"Failed to get process list size"}];
+      }
+      return @[];
+  }
+
+  NSInteger count = dataSize / sizeof(AudioObjectID);
+  AudioObjectID *processIDs = malloc(dataSize);
+
+  if (!processIDs) {
+    if (error) {
+        *error = [NSError errorWithDomain:@"AudioProcessMonitor"
+                                   code:-1
+                               userInfo:@{NSLocalizedDescriptionKey: @"Failed to allocate memory"}];
     }
-    
-    NSInteger count = dataSize / sizeof(AudioObjectID);
-    AudioObjectID *processIDs = malloc(dataSize);
-    
-    if (!processIDs) {
-        if (error) {
-            *error = [NSError errorWithDomain:@"AudioProcessMonitor"
-                                       code:-1
-                                   userInfo:@{NSLocalizedDescriptionKey: @"Failed to allocate memory"}];
-        }
-        return @[];
+    return @[];
+  }
+
+  status = AudioObjectGetPropertyData(kAudioObjectSystemObject,
+                                    &address,
+                                    0,
+                                    NULL,
+                                    &dataSize,
+                                    processIDs);
+
+  if (status != noErr) {
+    free(processIDs);
+    if (error) {
+        *error = [NSError errorWithDomain:@"AudioProcessMonitor"
+                                   code:status
+                               userInfo:@{NSLocalizedDescriptionKey: @"Failed to get process list"}];
     }
-    
-    status = AudioObjectGetPropertyData(kAudioObjectSystemObject,
+    return @[];
+  }
+
+  NSMutableArray<NSString *> *activeBundleIDs = [NSMutableArray array];
+
+  for (NSInteger i = 0; i < count; i++) {
+    AudioObjectID processID = processIDs[i];
+
+    // Check if process is running
+    address.mSelector = kAudioProcessPropertyIsRunningInput;
+    UInt32 isRunning = 0;
+    dataSize = sizeof(isRunning);
+
+    status = AudioObjectGetPropertyData(processID,
                                       &address,
                                       0,
                                       NULL,
                                       &dataSize,
-                                      processIDs);
-    
-    if (status != noErr) {
-        free(processIDs);
-        if (error) {
-            *error = [NSError errorWithDomain:@"AudioProcessMonitor"
-                                       code:status
-                                   userInfo:@{NSLocalizedDescriptionKey: @"Failed to get process list"}];
-        }
-        return @[];
+                                      &isRunning);
+
+    if (status != noErr || !isRunning) {
+        continue;
     }
-    
-    NSMutableArray<NSString *> *activeBundleIDs = [NSMutableArray array];
-    
-    for (NSInteger i = 0; i < count; i++) {
-        AudioObjectID processID = processIDs[i];
-        
-        // Check if process is running
-        address.mSelector = kAudioProcessPropertyIsRunningInput;
-        UInt32 isRunning = 0;
-        dataSize = sizeof(isRunning);
-        
-        status = AudioObjectGetPropertyData(processID,
-                                          &address,
-                                          0,
-                                          NULL,
-                                          &dataSize,
-                                          &isRunning);
-        
-        if (status != noErr || !isRunning) {
-            continue;
-        }
-        
-        // Get bundle ID
-        address.mSelector = kAudioProcessPropertyBundleID;
-        CFStringRef bundleIDRef = NULL;
-        dataSize = sizeof(bundleIDRef);
-        
-        status = AudioObjectGetPropertyData(processID,
-                                          &address,
-                                          0,
-                                          NULL,
-                                          &dataSize,
-                                          &bundleIDRef);
-        
-        if (status == noErr && bundleIDRef) {
-            NSString *bundleID = (NSString *)bundleIDRef;
-            if (bundleID.length > 0) {
-                [activeBundleIDs addObject:bundleID];
-            }
+
+    // Get bundle ID
+    address.mSelector = kAudioProcessPropertyBundleID;
+    CFStringRef bundleIDRef = NULL;
+    dataSize = sizeof(bundleIDRef);
+
+    status = AudioObjectGetPropertyData(processID,
+                                      &address,
+                                      0,
+                                      NULL,
+                                      &dataSize,
+                                      &bundleIDRef);
+
+    if (status == noErr && bundleIDRef) {
+        NSString *bundleID = (NSString *)bundleIDRef;
+        if (bundleID.length > 0) {
+            [activeBundleIDs addObject:bundleID];
         }
     }
-    
-    free(processIDs);
-    return [activeBundleIDs copy];
+  }
+
+  free(processIDs);
+  return [activeBundleIDs copy];
 }
 
 @end 
